293e843314d13ca924f43fb9402598890ee805d5
Olgierd "Allgreed" Kasprowicz <olgierd.kasprowicz@dynatrace.com>
2021-12-01 12:58:19 +0100

    Initial commit

---
+++ 1/first/src/main.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+
+pub fn main() anyerror!void {
+    //const filename = std.os.argv[1];
+
+    // TODO: make it dynamic!
+    //var file = try std.fs.cwd().openFile("inputs/test.txt", .{});
+    var file = try std.fs.cwd().openFile("inputs/real.txt", .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+
+    var counter: u32 = 0;
+    var buf: [1024]u8 = undefined;
+    var previous: u32 = undefined;
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        const measurement = try std.fmt.parseInt(u32, line, 10);
+
+        if (measurement > previous) {
+            counter += 1;
+        }
+
+        std.debug.print("buf {d} {d} \n", .{ measurement, previous });
+        previous = measurement;
+    }
+
+    std.log.info("{d}", .{counter});
+}
