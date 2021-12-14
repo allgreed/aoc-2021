c36157914825a90cd005d9db1449ace9f5625bd7
Olgierd "Allgreed" Kasprowicz <olgierd.kasprowicz@dynatrace.com>
2021-12-15 00:05:53 +0100

    Coded 14 finally xD

--- 14/src/main.zig
+++ 14/src/main.zig
@@ -25,6 +25,16 @@ pub fn main() anyerror!void {
 
     const polymer_line = (try in_stream.readUntilDelimiterOrEof(&buf, '\n')).?;
 
+    var counters = std.AutoArrayHashMap(u8, u64).init(allocator);
+
+    for (polymer_line) |c| {
+        var ble = try counters.getOrPut(c);
+        if (!ble.found_existing) {
+            ble.value_ptr.* = 0;
+        }
+        ble.value_ptr.* += 1;
+    }
+
     var pairs = std.StringArrayHashMap(u64).init(allocator);
     {
         var cp: [2]u8 = undefined;
@@ -61,19 +71,17 @@ pub fn main() anyerror!void {
         try rules.put(p1, c);
     }
 
-    print("{s}", .{pairs.keys()});
-    print("{d}", .{pairs.values()});
-
-    {
-        var it = pairs.iterator();
+    for (range(40)) |_| {
+        var _paris = try pairs.clone();
+        var it = _paris.iterator();
         while (it.next()) |pair| {
-            //const count = pair.value_ptr.*;
-            const count = 1;
-            pair.value_ptr.* -= count;
+            const count = pair.value_ptr.*;
             const key = pair.key_ptr.*;
 
+            var v = pairs.getPtr(key).?;
+            v.* -= count;
+
             const c = rules.get(key).?;
-            print("{s}", .{pair});
 
             var cp: [2]u8 = undefined;
 
@@ -89,7 +97,7 @@ pub fn main() anyerror!void {
             gpr.value_ptr.* += count;
 
             cp[0] = c;
-            cp[0] = key[1];
+            cp[1] = key[1];
             fuj = try allocator.alloc(u8, 2);
             std.mem.copy(u8, fuj[0..fuj.len], &cp);
             gpr = try pairs.getOrPut(fuj);
@@ -97,37 +105,33 @@ pub fn main() anyerror!void {
                 gpr.value_ptr.* = 0;
             }
             gpr.value_ptr.* += count;
+
+            var ble = try counters.getOrPut(c);
+            if (!ble.found_existing) {
+                ble.value_ptr.* = 0;
+            }
+            ble.value_ptr.* += count;
         }
     }
 
     print("{s}", .{pairs.keys()});
     print("{d}", .{pairs.values()});
 
-    //{
-    //var it = polymer.first;
-    //while (it) |node| : (it = node.next) {
-    //const c = node.data;
+    print("{s}", .{counters.keys()});
+    var values = counters.values();
+    print("{d}", .{values});
+    std.sort.sort(u64, values, {}, comptime std.sort.asc(u64));
 
-    //const gpr = try counters.getOrPut(c);
-    //if (!gpr.found_existing) {
-    //gpr.value_ptr.* = 0;
-    //}
-    //gpr.value_ptr.* += 1;
-    //}
-    //}
+    const maxx = values[values.len - 1];
+    var minn: u64 = undefined;
+    for (values) |v| {
+        if (v != 0) {
+            minn = v;
+            break;
+        }
+    }
 
-    //var maxx: u64 = 0;
-    //var minn: u64 = std.math.maxInt(u64);
-    //{
-    //var it = counters.valueIterator();
-    //while (it.next()) |_v| {
-    //const v = _v.*;
-    //maxx = std.math.max(maxx, v);
-    //minn = std.math.min(minn, v);
-    //}
-    //}
-
-    //print("{d}", .{maxx - minn});
+    print("{d}", .{maxx - minn});
 }
 
 fn range(count: usize) []const u0 {
